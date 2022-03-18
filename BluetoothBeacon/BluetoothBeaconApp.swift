dcca15eeed3237e98e2e5819c368bad16496f636
Aung Yee <lyndon.lso@gmail.com>
2022-03-18 10:20:07 +0000

    Initial Commit

---
+++ BluetoothBeacon/BluetoothBeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothBeaconApp.swift
+//  BluetoothBeacon
+//
+//  Created by Aung Yee on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothBeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothBeacon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BluetoothBeacon
+//
+//  Created by Aung Yee on 18/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
